8d744136bf94c6c0074b565cad03ead7892217bd
Icker Villalon <icker.villalon@gmail.com>
2026-03-30 08:01:01 -0600

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        """
+        Dynamic Programming
+        Time: O(n)
+        Space: O(1)
+        """
+        res = max(nums)
+        curMax, curMin = 1, 1
+        for num in nums:
+            tmp =  curMax * num
+            curMax = max(curMax * num, curMin * num, num)
+            curMin = min(tmp, curMin * num, num)
+            res = max(res, curMax)
+        return res
