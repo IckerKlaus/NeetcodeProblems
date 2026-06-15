275c109094a6b19e66d5bb11baea0abe744943e9
Icker Villalon <icker.villalon@gmail.com>
2026-06-15 10:03:32 -0600

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Time: O(n)
+        Space: O(n)
+        5
+        1   2   3   4
+                i
+        diff = 2
+        hashmap = 1:0, 2:1,  #num:idx
+        return [i, hashmap[diff]]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[nums[i]] = i
