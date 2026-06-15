58dae73fd8102f7e46eee7e12e8598772525275e
Icker Villalon <icker.villalon@gmail.com>
2026-06-15 10:41:19 -0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        Time: O(n)
+        Space: O(n)
+        hashmap = key:val -> arrASCII:[word1, ...]
+        iterate through strs
+            convert str in arr ascii
+            hashmap[tuple(arrASCII)].append(str)
+        return list(hashmap.values())
+        """
+        hashmap = defaultdict(list)
+        for s in strs:
+            arrASCII = [0] * 26
+            for c in s:
+                arrASCII[ord(c) - ord('a')] += 1
+            hashmap[tuple(arrASCII)].append(s)
+        return list(hashmap.values())
